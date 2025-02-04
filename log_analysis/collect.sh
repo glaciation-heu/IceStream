688bada225688c7da5c8852e057b94defa6b3c47
Piao <guangyuan.piao@dell.com>
2025-02-04 13:15:11 +0000

    update collect shell script

---
+++ log_analysis/collect.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+curl -X POST "http://grafana.integration/api/ds/query?ds_type=loki&requestId=Q109_1" \
+    -H "Content-Type: application/json" \
+    -d '{
+            "queries":[
+	        {"datasource":{
+		    "type":"loki",
+		    "uid":"P8E80F9AEF21F6940"
+	            },
+		    "editorMode":"code",
+		    "expr":"{namespace=~\"dkg-engine\", container =~\"metadata-service\"} |= \"\"",
+		    "queryType":"range",
+		    "refId":"A",
+		    "maxLines":1000,
+		    "legendFormat":"",
+		    "datasourceId":2,
+		    "intervalMs":30000,
+		    "maxDataPoints":567}
+            ],
+	    "from":"1738638979765",
+	    "to":"1738660579765"
+        }'
+
